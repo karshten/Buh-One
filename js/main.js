9f345e2adc3d1e0106920837d86c1acca6ebc18e
oskar shtefan <oskar.shtefan@gmail.com>
2022-03-14 16:44:44 +0600

    test menu

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+const menuBtn = document.querySelector('.menuBtn');
+const menu = document.querySelector('.menu__mobile__content')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+
+  if(!menuOpen) {
+    menu.classList.add('menu__open');
+    menuBtn.classList.add('openBtn');
+    menuOpen = true;
+  } else {
+    menu.classList.remove('menu__open');
+    menuBtn.classList.remove('openBtn');
+    menuOpen = false;
+  }
+});
